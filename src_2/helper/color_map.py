e83192cd1950a0127e1c427f561bef9be9627148
DucVinhk5 <vinhn6514@gmail.com>
2025-12-23 14:02:44 +0700

    Xóa thư mục src_2

--- src_2/helper/color_map.py
+++
@@ -1,8 +0,0 @@
-# ---- Màu ANSI ----
-GREEN = "\033[92m"  # Success
-RED = "\033[91m"  # Fail
-YELLOW = "\033[93m"  # Retry / Warning
-RESET = "\033[0m"
-
-# ---- Icon & màu map ----
-STATUS_MAP = {"Success": ("[+]", GREEN), "Fail": ("[-]", RED), "Retry": ("[!]", YELLOW)}
